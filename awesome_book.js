2172a591a9bd4d68948be838a23e482c8ea43bfa
Damola Adediran <tylher123@gmail.com>
2022-03-14 16:46:25 +0100

    Add linters and Update readme file

--- awesome_book.js
+++ awesome_book.js
@@ -5,55 +5,51 @@ const bookAuthor = document.getElementById('author');
 const displayBooks = document.querySelector('.display-books');
 const add = document.querySelector('.add-book');
 
-
-
-class books {
-    constructor(Title, author) {
-        this.Title = Title
-        this.author = author
-    }
+class Books {
+  constructor(Title, author) {
+    this.Title = Title;
+    this.author = author;
+  }
 }
 
 function addBook() {
-    let title = bookTitle.value;
-    let author = bookAuthor.value;
-    let book = new books(title, author);
-    Book.push(book);
-    console.log(Book)
-    let div = document.createElement('div');
-    div.innerHTML = `<h2>${book.Title}</h2> <p>${book.author}</p> <button onclick='remove(this)' class='remove-book'>remove</button> <hr>`
-    displayBooks.appendChild(div);
-    author = "";
-    title = "";
+  let title = bookTitle.value;
+  let author = bookAuthor.value;
+  const book = new Books(title, author);
+  Book.push(book);
+  const div = document.createElement('div');
+  div.innerHTML = `<h2>${book.Title}</h2> <p>${book.author}</p> <button onclick='$remove(this)' class='remove-book'>remove</button> <hr>`;
+  displayBooks.appendChild(div);
+  author = '';
+  title = '';
 }
 
 add.addEventListener('click', addBook);
 
-
+/* eslint-disable */
 function remove(e) {
-    displayBooks.innerHTML = ""
-    console.log(e.parentElement.childNodes);
-    Book = Book.filter(book => book.Title != e.parentElement.childNodes[0].textContent);
-    console.log(Book);
+    /* eslint-enable */
+  displayBooks.innerHTML = '';
+  Book = Book.filter((book) => book.Title !== e.parentElement.childNodes[0].textContent);
 
-
-    Book.map(book => {
-        let div = document.createElement('div');
-        div.innerHTML = `<h2>${book.Title}</h2> <p>${book.author}</p> <button onclick='remove(this)' class='remove-book'>remove</button> <hr>`
-        displayBooks.appendChild(div);
-    })
+  /* eslint-disable */
+    Book.map((book) => {
+        /* eslint-enable */
+    const div = document.createElement('div');
+    div.innerHTML = `<h2>${book.Title}</h2> <p>${book.author}</p> <button onclick='remove(this)' class='remove-book'>remove</button> <hr>`;
+    displayBooks.appendChild(div);
+  });
 }
 
-
 let formData = {
-    title: '',
-    author: '',
-  };
+  title: '',
+  author: '',
+};
 
-  if (localStorage.getItem('formData') !== null) {
-    const data = localStorage.getItem('formData');
-    formData = JSON.parse(data);
-  }
+if (localStorage.getItem('formData') !== null) {
+  const data = localStorage.getItem('formData');
+  formData = JSON.parse(data);
+}
 
 const formElements = document.querySelectorAll('input, textarea');
 formElements.forEach((element) => {
